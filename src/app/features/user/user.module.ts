973b171b3eb1deef1ad580043fde7070f0338d48
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2024-11-13 10:31:30 +0100

    new feature: login interface

--- src/app/features/user/user.module.ts
+++ src/app/features/user/user.module.ts
@@ -5,10 +5,12 @@ import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
 import { RegisterComponent } from './register/register.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
     UserComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
