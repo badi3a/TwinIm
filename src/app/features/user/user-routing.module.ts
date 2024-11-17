5868ee482f7541b0eae4eb08d7926ef9b7afe1ba
Taher <medtaher131@gmail.com>
2024-11-17 21:44:03 +0100

    new header

--- src/app/features/user/user-routing.module.ts
+++ src/app/features/user/user-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user.component';
 import {RegisterComponent} from "./register/register.component";
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: UserComponent },
-  {path:'register', component: RegisterComponent},];
+  {path:'register', component: RegisterComponent},
+  {path:'login', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
