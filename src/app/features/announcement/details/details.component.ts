0a09208a3f52fd93fc7e53361bbc33fc30f0736c
louay abidi <louay.abidi@esprit.tn>
2024-11-26 22:34:35 +0100

    details with update annoucement

--- src/app/features/announcement/details/details.component.ts
+++ src/app/features/announcement/details/details.component.ts
@@ -12,9 +12,12 @@ import { Announcement } from 'src/app/core/models/announcement';
 export class DetailsComponent implements OnInit{
 
   announcement : Announcement;
+  isEditing: boolean = false; // Pour savoir si on est en mode édition
+  imagePreview: string | ArrayBuffer | null = null;
+
   
   constructor(private activatedRoute: ActivatedRoute 
-    , private announcementService: AnnouncementService , private router: Router) {
+    , private announcementService: AnnouncementService , public router: Router) {
   }
     
     ngOnInit() {
@@ -32,6 +35,40 @@ export class DetailsComponent implements OnInit{
 
 
       )
+    }
+
+
+
+
+    toggleEdit(): void {
+      this.isEditing = !this.isEditing; // Permet de passer en mode édition ou non
+    }
+
+
+
+    onImageSelect(event: any): void {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.imagePreview = e.target.result;
+          this.announcement.picture = this.imagePreview as string; // Mettre à jour l'image dans l'annonce
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+
+
+
+    updateAnnouncement(): void {
+      const id = this.activatedRoute.snapshot.params['id'];
+      this.announcement.datePublication = new Date(); // Mettre à jour la date de publication
+      this.announcementService.updateAnnouncement(id, this.announcement).subscribe(
+        () => {
+          this.isEditing = false; // Désactive le mode édition
+          this.router.navigate(['/announcement/list']); // Redirige vers la liste des annonces
+        }
+      );
     }
 
 
