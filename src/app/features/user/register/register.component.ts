e78d6b9b52d374e5f4d5f08f04c8dc068e59205b
zeineb-m <zeineb.maatalli@esprit.tn>
2024-12-05 14:15:15 +0100

    sign up

--- src/app/features/user/register/register.component.ts
+++ src/app/features/user/register/register.component.ts
@@ -14,12 +14,17 @@ export class RegisterComponent implements OnInit {
   // FormGroup: class that match the form tag in the Template && is a collection of inputs
      formRegister: FormGroup;
      user:User = new User();
+     imagePreview: string | ArrayBuffer | null = null;
      constructor(private userService: UserService, private router: Router) {}
      ngOnInit() {
        this.formRegister= new FormGroup(
          //FormControl: class that match an input in the form
          {firstName: new FormControl('',[Validators.required, Validators.minLength(3)]),
          lastName: new FormControl('',[Validators.required, Validators.minLength(3)]),
+         password: new FormControl('',[Validators.required, Validators.minLength(8)]),
+         login: new FormControl('', [Validators.required, Validators.email]),
+         birthday: new FormControl('', [Validators.required]),
+         picture: new FormControl('', []),
          address: new FormGroup(
            { street: new FormControl('',[Validators.required]),
              city: new FormControl('',[Validators.required]),
@@ -46,12 +51,35 @@ export class RegisterComponent implements OnInit {
        return (this.formRegister.get('phoneNumbers') as FormArray);
       }
      save(){
+      this.savePicture();
        //backend
        this.user = this.formRegister.getRawValue();
        this.userService.addUser(this.user).subscribe(
-         (data:User)=>{this.router.navigateByUrl('user/profile/${data.id}');}
+         (data:User)=>{this.router.navigateByUrl(`user/profile/${data.id}`);}
        )
        //get the set of value of each input in the form
         console.log(this.formRegister.getRawValue())
      }
+     onImageSelect(event: any) {
+      const file = event.target.files[0]; // Récupérer le fichier sélectionné
+      if (file) {
+        if (!file.type.startsWith('image/')) {
+          alert('Veuillez sélectionner un fichier image valide.');
+          return;
+        }
+    
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.imagePreview = e.target.result; // Prévisualiser l'image
+          // Stocker l'image encodée en base64 dans le formulaire
+          this.formRegister.get('picture')?.setValue(e.target.result);
+        };
+        reader.readAsDataURL(file); // Convertir le fichier en base64
+      }
+    }
+    
+    savePicture()
+    {
+  
+    }
 }
